00d83e884e50e11dbcd5fcc20c2c63af8d52a73b
Danilo <randomlittlest@gmail.com>
2025-06-13 20:27:21 -0300

    inicio da homepage e sua config

---
+++ src/components/layout/slide1.vue
@@ -0,0 +1,40 @@
+<script>
+    import PrimaryButton from '../utilities/PrimaryButton.vue';
+    import SecundaryButton from '../utilities/SecundaryButton.vue';
+    export default{
+        components:{
+            PrimaryButton,
+            SecundaryButton
+        },
+    }
+</script>
+
+<template>
+    <div class="conteiner">
+        <div class="texto">
+            <h1>Transforme</h1>
+            <h2>algo que sobra em algo que</h2> 
+            <h1>soma</h1>
+        </div>
+        <img src="@/assets/icons/Imagem-Principal-Home.png" id="initial-image"> 
+    </div>
+</template>
+
+<style>
+    .conteiner{
+        display: grid;
+        grid-template-columns: 2fr 2fr; /* Ele divide os elementos proporcionalmente na tela*/
+    }
+    #initial-image{
+        width: 100%;
+        height: 100%
+    }
+    
+    .texto{
+        position: absolute;
+        top: 10%;
+        left: 10%;
+        padding: 200px;
+        color: #fff;
+    } 
+</style>
--- src/pages/Home_Page.vue
+++ src/pages/Home_Page.vue
@@ -1,9 +1,16 @@
 <script setup>
     import navbar from '../components/layout/Navbar.vue';
+    import slide1 from '../components/layout/slide1.vue';
 </script>
 
 <template>
     <navbar/>
+    <div>
+        <slide1/>
+        <slide2/>
+        <slide3/>
+        <slide4/>
+    </div>
 </template>
 
 <style>
